a07f778077c47f2a692f63eeca6387162b5c7821
mmumby <meredith.mumby@outlook.com>
2017-09-24 22:26:38 +0000

    rendered api data on enter event

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,61 +1,12 @@
 import React, { Component } from 'react';
 import '../styles/joblist.css';
 import { createStore } from 'redux';
+// import superagent from 'superagent';
 
 class JobList extends Component {
   render() {
     return (
       <div className="joblist-container">
-      <p>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus culpa nostrum explicabo modi magni suscipit ipsam ducimus non iste. Iste quam quaerat doloremque aliquam commodi architecto qui voluptates laboriosam, fuga?
-        <br/><br/><br/>
-      </p>
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import '../styles/navbar.css';
+import superagent from 'superagent';
 
 class SearchBar extends Component {
   constructor(){
     super()
     this.state ={
-      searchValue: ''
+      searchValue: '',
+      feed: []
     }
   }
 
@@ -13,12 +15,27 @@ class SearchBar extends Component {
     if (event.key === 'Enter') {
       console.log("searchJobs: "+this.state.searchValue);
       event.target.value = '';
+
+      superagent
+        .get('http://api.dataatwork.org/v1/jobs')
+        .query(null)
+        .set('Accept', 'application/json')
+        .end((err, response) => {
+          if (err) {
+            console.log('Error:' + err)
+            return
+          }
+          console.log('Response:' + JSON.stringify(response.body))
+          this.setState({
+            feed: response.body
+          })
+        })
     }
   }
 
   updateSearch(event){
     console.log("updateSearch: "+event.target.value)
-
+// will change to redux.
     this.setState({
       searchValue: event.target.value
     })
@@ -28,8 +45,17 @@ class SearchBar extends Component {
     return (
       <div>
         <input onKeyDown={this.searchJobsKeyDown.bind(this)} onChange={this.updateSearch.bind(this)} type="text" className="search-bar" placeholder="&#xf002;  Search and hit ENTER"/>
+      <p> jobList </p>
+      { this.state.feed.map((post, i) => {
+          return (
+            <div key={post.uuid}>
+              <p> {post.title} </p>
+            </div>
+          )
+        })
+      }
       </div>
-    );
+    )
   }
 }
 
