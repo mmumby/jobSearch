ca65c46b8697880b916643d63ed774ccccbfa461
mmumby <meredith.mumby@outlook.com>
2017-09-24 20:50:11 +0000

    added search event listeners to searchbox

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import '../styles/navbar.css';
 
 class SearchBar extends Component {
+  constructor(){
+    super()
+    this.state ={
+      searchValue: ''
+    }
+  }
+
+  searchJobs(){
+    console.log("searchJobs: "+this.state.searchValue);
+  }
+
+  updateSearch(event){
+    console.log("updateSearch: "+event.target.value)
+
+    this.setState({
+      searchValue: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
-        <input className="search-bar" placeholder="&#xf002;  Search..."/>
+        <input onChange={this.updateSearch.bind(this)} type="text" className="search-bar" placeholder="&#xf002;  Search..."/>
+        <button onClick={this.searchJobs.bind(this)}>Search</button>
       </div>
     );
   }
