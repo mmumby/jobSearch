491c3f24a05770a7f87fc9a2ab312b74b3e07606
mmumby <meredith.mumby@outlook.com>
2017-09-28 04:08:22 +0000

    added scrolltop button/functionality

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 import Search from './Search';
 import Header from './Header';
+import ScrollTop from './ScrollTop';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
         <div className="App">
             <Header />
             <Search />
+            <ScrollTop />
         </div>
     );
   }
